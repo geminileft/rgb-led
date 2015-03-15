ef2f969b28f7ed1d31f0b18b1e0dd0c8585db7bd
Matt Galaviz <matthew.galaviz@gmail.com>
2015-03-14 19:00:03 -0700

    working code

--- RGB_LED.ino
+++ RGB_LED.ino
@@ -1,18 +1,27 @@
 
-int redPin = 11; int greenPin = 10; int bluePin = 9;
+int redPin = 9; int greenPin = 10; int bluePin = 11;
 //uncomment this line if using a Common Anode LED //#define COMMON_ANODE
 void setup() {
+  //white, blue (anode), green, orange
+ //red, anode, green, blue
 pinMode(redPin, OUTPUT); pinMode(greenPin, OUTPUT); pinMode(bluePin, OUTPUT);
 }
 void loop() {
+//setColor(255, 0, 0); //r
+//setColor(0, 255, 0); //g
+//setColor(0, 0, 255); //b
+setColor(255, 0, 255); //rb
 //setColor(0, 0, 0); //b
-//setColor(255, 0, 0); //gb
-//setColor(0, 255, 0); //rb
-//setColor(255, 255, 0); //rgb
-//setColor(0, 0, 255); //fail
-//setColor(0, 255, 255); //fail
-//setColor(255, 0, 255); //fail
 //setColor(255, 255, 255); //fail
+//setColor(0, 0, 255); //b
+//setColor(0, 255, 0); //b
+//setColor(255, 0, 0); //r
+//setColor(255, 255, 0); //b
+//setColor(225, 0, 255); //b
+//setColor(0, 255, 255); //fail
+//setColor(255, 255, 0); //r
+//setColor(255, 0, 0); //r
+//setColor(0, 255, 0); //r
 }
 
 void setColor(int red, int green, int blue) {
