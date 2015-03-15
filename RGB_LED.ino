a11a76a6bdf3489a3919d602ffdd0df82bebfa20
Matt Galaviz <matthew.galaviz@gmail.com>
2015-03-14 17:35:14 -0700

    updated with test cases

--- RGB_LED.ino
+++ RGB_LED.ino
@@ -5,17 +5,16 @@ void setup() {
 pinMode(redPin, OUTPUT); pinMode(greenPin, OUTPUT); pinMode(bluePin, OUTPUT);
 }
 void loop() {
-setColor(255, 0, 0); // red delay(1000);
-//setColor(0, 255, 0); // green delay(1000);
-//setColor(0, 0, 255); // blue delay(1000);
-//setColor(255, 255, 0); // yellow delay(1000);
-//setColor(80, 0, 80); // purple delay(1000);
-//setColor(0, 255, 255); // aqua delay(1000);
+//setColor(0, 0, 0); //b
+//setColor(255, 0, 0); //gb
+//setColor(0, 255, 0); //rb
+//setColor(255, 255, 0); //rgb
+//setColor(0, 0, 255); //fail
+//setColor(0, 255, 255); //fail
+//setColor(255, 0, 255); //fail
+//setColor(255, 255, 255); //fail
 }
 
 void setColor(int red, int green, int blue) {
-#ifdef COMMON_ANODE red = 255 - red;
-green = 255 - green; blue = 255 - blue;
-#endif
 analogWrite(redPin, red); analogWrite(greenPin, green); analogWrite(bluePin, blue);
 }
